e143ba3fab1ea98471150347925724d551f5f70f
Emillie York <emillieyork@mmc.school.nz>
2019-06-20 15:05:48 +1200

    Changed colour of form

--- FlyLadybug/FrmLadybug.Designer.cs
+++ FlyLadybug/FrmLadybug.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
             this.ClientSize = new System.Drawing.Size(784, 411);
             this.Name = "FrmLadybug";
             this.Text = "Fly Ladybug ";
