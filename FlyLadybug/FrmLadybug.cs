86f33b76d09cd46c0e9d18c7d85d778b6c496b1c
Emillie York <emillieyork@mmc.school.nz>
2019-06-27 14:48:01 +1200

    added multiple balls (7)

--- FlyLadybug/FrmLadybug.cs
+++ FlyLadybug/FrmLadybug.cs
@@ -31,7 +31,7 @@ namespace FlyLadybug
         int x2 = 540, y2 = 250; //starting position of Ladybug
         //Load our two images from the bin/debug folder
         Image ladybug = Image.FromFile(Application.StartupPath + @"/ladybug.png");
-        Image ball = Image.FromFile(Application.StartupPath + @"/dragon.png");
+        Image ball1 = Image.FromFile(Application.StartupPath + @"/dragon.png");
 
         private void PnlGame_Paint(object sender, PaintEventArgs e)
         {
@@ -40,8 +40,13 @@ namespace FlyLadybug
             //use the DrawImage method to draw the ladybug on the panel 
             g.DrawImage(ladybug, area);
             //use the DrawImage method to draw the dragon on the panel
-            g.DrawImage(ball, area1);
-           
+            g.DrawImage(ball1, area1);
+            g.DrawImage(ball1, area2);
+            g.DrawImage(ball1, area3);
+            g.DrawImage(ball1, area4);
+            g.DrawImage(ball1, area5);
+            g.DrawImage(ball1, area6);
+            g.DrawImage(ball1, area7);
         }
 
         
@@ -49,8 +54,14 @@ namespace FlyLadybug
         {
             InitializeComponent();
             area = new Rectangle(x2, y2, 30, 30); //Ladybug's rectangle
-            area1 = new Rectangle(x, y, 20, 20); //ball's rectangle
-            speed1 = speed.Next(5, 10);//ball's speed will be between 5 and 10
+            area1 = new Rectangle(x, y, 20, 20); //ball1's rectangle
+            area2 = new Rectangle(x + 70, y, 40, 40);//ball2's reactangle
+            area3 = new Rectangle(x + 140, y, 40, 40);//ball3's rectangle
+            area4 = new Rectangle(x + 210, y, 40, 40);//ball4's rectangle
+            area5 = new Rectangle(x + 280, y, 40, 40);//ball5's rectangle
+            area6 = new Rectangle(x + 350, y, 40, 40);//ball6's rectangle
+            area7 = new Rectangle(x + 420, y, 40, 40);//ball7's rectangle
+            speed1 = speed.Next(5, 10);//ball1's speed will be between 5 and 10
            
         }
     }
