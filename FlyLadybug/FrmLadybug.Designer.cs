e74333cb9f35fb6177e200ae46a5ba3adb4038d0
Emillie York <emillieyork@mmc.school.nz>
2019-07-02 10:29:55 +1200

    added lables and textboxs to form: score, lives and name

--- FlyLadybug/FrmLadybug.Designer.cs
+++ FlyLadybug/FrmLadybug.Designer.cs
@@ -32,6 +32,12 @@
             this.PnlGame = new System.Windows.Forms.Panel();
             this.TmrBall = new System.Windows.Forms.Timer(this.components);
             this.TmrLadybug = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txtlives = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // PnlGame
@@ -53,12 +59,69 @@
             this.TmrLadybug.Interval = 50;
             this.TmrLadybug.Tick += new System.EventHandler(this.TmrLadybug_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(635, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(638, 176);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(100, 20);
+            this.TxtName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(637, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "score";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(637, 242);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(13, 13);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(635, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lives";
+            // 
+            // Txtlives
+            // 
+            this.Txtlives.Location = new System.Drawing.Point(640, 299);
+            this.Txtlives.Name = "Txtlives";
+            this.Txtlives.Size = new System.Drawing.Size(100, 20);
+            this.Txtlives.TabIndex = 6;
+            this.Txtlives.Text = "5";
+            // 
             // FrmLadybug
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Txtlives);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.PnlGame);
             this.KeyPreview = true;
             this.Name = "FrmLadybug";
@@ -66,6 +129,7 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmLadybug_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FrmLadybug_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +138,12 @@
         private System.Windows.Forms.Panel PnlGame;
         private System.Windows.Forms.Timer TmrBall;
         private System.Windows.Forms.Timer TmrLadybug;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Txtlives;
     }
 }
 
